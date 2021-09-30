8ed6f31c6d9a16607cbed1aa111c059d65c625ea
Your Name <you@example.com>
2021-09-30 15:47:37 +0900

    answer-by-mentor

--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import ReactDOM from 'react-dom';
 import { Board } from './component/Board';
 import './index.css';
 import { calculateWinner } from './utility'
 
 const Game = () => {
-    // const [history, setHistory] = useState({squares: Array(9).fill(null)});
     // const [stepNum, setStepNum] = useState(0);
-    const [xIsNext, setXIsNext] = useState(true);
+    // const [xIsNext, setXIsNext] = useState(true);
     // const [isDesc, setIsDesc] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(''));
+    // const [squares, setSquares] = useState(Array(9).fill(''));
+    const [state, setState] = useState({
+        history: [{
+            squares: (Array(9).fill('')),
+        }],
+        xIsNext: true,
+    });
 
     const fillWithXOrO = (num) => {
-        if (calculateWinner(squares) || squares[num]) {
-            console.log(squares[0]);
-            return;
-        }
-        setSquares(prevValues => {
-            prevValues[num] = xIsNext ? 'X' : 'O';
-            setXIsNext(prevXIsNext => !prevXIsNext);
-            return prevValues.concat();
+        // const current = history.squares[history.squares.length - 1];
+        // const squares = current.concat();
+        // if (calculateWinner(squares) || squares[num]) {
+        //     console.log(squares[0]);
+        //     return;
+        // }
+        setState(prevValues => {
+            prevValues.squares[num] = prevValues.xIsNext ? 'X' : 'O';
+            prevValues.xIsNext = !prevValues.xIsNext;
+            // return prevValues.concat();
         });
     }
-    useEffect(()=>{
-console.log(squares)
-    }, [squares])
+
+    const current = useMemo(() => 
+    (state.history[state.history.length - 1]), 
+    [state.history])
+
+    const [h, setH ] = useState();
+
+    useEffect(() =>{
+        setH(state.history[state.history.length - 1])
+    }, [state.history])
+    []
+    setState({history: []})
+    // const a = state.history[state.history.length - 1]
 
     return(
         <div className="game">
             <div className="game-board">
                 <Board 
                     onClick={ fillWithXOrO }
-                    squares={ squares }
-                    xIsNext={ xIsNext }
+                    squares={ history.squares }
+                    xIsNext={ history.xIsNext }
                 />
             </div>
             <div className="game-info">
